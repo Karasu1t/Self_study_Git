d103194ddb7679f8f2dfd5a99519136b379a5f24
Masaru Ogasawara <massaru44@gmail.com>
2024-12-20 22:58:01 +0900

    CICD Automation Blue/GeenDeploy

--- IaC(Terrafrom)/CICDAutomation/envs/dev/codedeploy.tf
+++ IaC(Terrafrom)/CICDAutomation/envs/dev/codedeploy.tf
@@ -41,11 +41,11 @@ resource "aws_codedeploy_deployment_group" "ecs_deployment_group" {
 
   blue_green_deployment_config {
     deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
+      action_on_timeout = "STOP_DEPLOYMENT"
     }
 
     terminate_blue_instances_on_deployment_success {
-      action                           = "TERMINATE"
+      action                           = "KEEP_ALIVE"
       termination_wait_time_in_minutes = 5
     }
   }
